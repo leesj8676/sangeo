cf070e343e3ae32ec61ee26ed80485886bf47dca
leesj8676 <leesj8676@gmail.com>
2022-08-12 16:25:18 +0900

    Chore : Delete strictMode to join session only ocne

--- front-developing/src/index.js
+++ front-developing/src/index.js
@@ -41,14 +41,12 @@ if(localStorage.getItem("Authorization")){
   store.dispatch({type:'LOG_IN', user: jwtDecode(localStorage.getItem("Authorization"))});
 }
 root.render(
-  <React.StrictMode>
     <Provider store={ store }>
     <App 
     authService={authService}
     imageUploader= {imageUploader}
     EditorboxRepository= {EditorboxRepository} />
     </Provider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
