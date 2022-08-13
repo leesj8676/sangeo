1a33a4e620f2c406efab48e0b28bbae4502f20a3
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-08-13 19:19:24 +0900

    Revert "Merge branch 'feature/css' into fe"
    This reverts commit a90c7827d529a667e146b2febed59327b69ab94d, reversing
    changes made to 0cab3f49d19d7d1c6b33efaf91ed527a46d0040a.

--- front-developing/src/components/counselorlist/counselorlist.jsx
+++ front-developing/src/components/counselorlist/counselorlist.jsx
@@ -17,4 +17,3 @@ const CounselorList = ({cards}) => {
 };
 
 export default CounselorList;
-
--- front-developing/src/pages/HomePage.jsx
+++ front-developing/src/pages/HomePage.jsx
@@ -1,30 +1,31 @@
 import Stack from 'react-bootstrap/Stack';
 import React from 'react';
-// import styles from './HomePage.module.css';
-import './HomePage.css';
-
-
+import styles from './HomePage.module.css';
 
 function HomePage() {
 
   return (
-   <div className="wrapper">
-      <div className="container">
-      <div className="neon-container">
-      <div className="neon">
-        <p>미술심리상담 <br/>  상어로 시작하세요</p>
-     </div>
-    </div>
-    </div>
-
-    
     
 
-    {/* <Stack gap={2} className="col-md-8 mx-auto">
+    <div>
+
+      <section className={styles.back} >
+      {/* <img src = "light3.png" alt ="main_back"/> */}
+      <div className={styles.neon}>
+        <p>     미술심리상담 <br/>  상어로 시작하세요</p>
+        
+      </div>
+          
+
+      </section>
+
+
+       
+    <Stack gap={2} className="col-md-8 mx-auto">
 
       <img src="sangeo_banner.png" alt="main_logo"></img>
 
-      </Stack> */}
+      </Stack>
 
 </div>
 
