9e6432896c64d485ff70485b50159c783fff6aee
leesj8676 <65400677+leesj8676@users.noreply.github.com>
2022-08-14 14:40:39 +0900

    Style : Change logo src with using PUBLIC_URL in Navbar Component

--- front-developing/src/components/header/Navbar.jsx
+++ front-developing/src/components/header/Navbar.jsx
@@ -57,7 +57,7 @@ function NavigationBar({authService}) {
     <Navbar key="lg" expand="lg" className={styles.color}>
     <Container fluid>
       <Navbar.Brand className="col-4 ps-3" href="/">
-      <img src="http://localhost:3000/sangeo_logo.png" width="250" height="100" alt="logo"></img>
+      <img src="https://i7e207.p.ssafy.io/sangeo_logo.png" width="250" height="100" alt="logo"></img>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
       <Navbar.Offcanvas
