128de945723aedbc59ec861bb31bf84c6af50725
leesj8676 <leesj8676@gmail.com>
2022-08-16 15:03:39 +0900

    Style : Change CounScheduleBox UI (like UserSchduleBox)

--- front-developing/src/pages/CounMyPage.jsx
+++ front-developing/src/pages/CounMyPage.jsx
@@ -3,29 +3,19 @@ import CounProfile from '../components/profile/CounProfile';
 import CounSchedulebox from '../components/schedule/CounSchedulebox'
 import CounTime from '../components/schedule/CounTime'
 import CounTimeUpdate from '../components/schedule/CounTimeUpdate'
-import CounRequest from '../components/schedule/CounRequestbox'
 
 function CounMyPage ({imageUploader}){
     const [data,setData] = useState(false) //상담시간, 상담시간수정 토글
-    const [confirm,setConfirm] = useState("상담목록") //승인된 상담 승인되지 않은 상담
 
-    function changeConfirm1(){
-        setConfirm("상담목록")
-    }
-    function changeConfirm2(){
-        setConfirm("상담요청")
-    }
     return (
       <div>
         <h2 className='text-center'>마이페이지</h2>
         <CounProfile imageUploader={imageUploader} />
-        <div>
+        {/* <div>
             { data===false ? <CounTime setData={setData}/> : <CounTimeUpdate setData={setData}/>}
-        </div>
-        <button onClick={changeConfirm1}>상담목록</button>
-        <button onClick={changeConfirm2}>상담요청</button>
+        </div> */}
         <div>
-            { confirm==="상담목록" ? <CounSchedulebox/> : <CounRequest/>}
+            {  <CounSchedulebox/> }
         </div>
         
       </div>
